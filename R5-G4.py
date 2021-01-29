539aee249505b5314d766b8c0951ed7a12605994
Dennis Capone <dennis.d.cap@gmail.com>
2021-01-28 21:10:04 -0700

    Added a functionality to roll for obligation.

--- R5-G4.py
+++ R5-G4.py
@@ -1,3 +1,5 @@
+import random
+
 import discord
 
 client = discord.Client()
@@ -5,13 +7,16 @@ client = discord.Client()
 
 @client.event
 async def on_ready():
-    print("{0.user}: BEEP BEEP!".format(client))
+    print("{0.user}: Bwoooooop!".format(client))
 
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
+
+    if message.content.startswith('$R5: Obligation'):
+        await message.channel.send("*Whistle*: " + str(random.randint(1, 100)))
 
     if message.content.startswith('$R5-G4'):
         await message.channel.send("BEEEEeeeep!")
