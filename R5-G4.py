5f39d13b2a1f58fd9d8636c1a94c1fa8939246fc
Dennis <capone@alamon.com>
2021-01-28 17:00:12 -0700

    Committing starter files

---
+++ R5-G4.py
@@ -0,0 +1,20 @@
+import discord
+
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print("{0.user}: BEEP BEEP!".format(client))
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$R5-G4'):
+        await message.channel.send("BEEEEeeeep!")
+
+
+client.run()
