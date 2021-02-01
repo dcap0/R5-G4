65d7338c470614255e4582c1de77c280735510c5
Dennis Capone <capone@alamon.com>
2021-02-01 14:53:27 -0700

    Started implementing sounds on reactions.

--- R5-G4.py
+++ R5-G4.py
@@ -1,15 +1,36 @@
 import discord
 from discord.ext import commands
 import random
+import os
+
+RES_DIR = os.path.dirname(os.path.abspath(__file__)) + "/res/"
 
 print("Vwooooo!")
 
 R5 = commands.Bot(command_prefix='R5:')
 
+channelSounds = ['vc1.mp3', 'vc2.mp3', 'vc3.mp3']
+randomSounds = ['r1.mp3', 'r2.mp3', 'r3.mp3', 'r4.mp3', 'r5.mp3', 'r6.mp3', 'r7.mp3', 'r8.mp3']
+err = 'err.mp3'
+
+
+def returnChannelSound():
+    return RES_DIR + channelSounds[random.randint(0, 2)]
+
+
+def returnRandomSound():
+    return RES_DIR + randomSounds[random.randint(0, 7)]
+
 
 @R5.command('obligation')
 async def obligation(ctx):
-    await ctx.channel.send(str(random.randint(1, 100)))
+    voice = discord.utils.get(R5.voice_clients, guild=ctx.guild)
+    try:
+        await voice.play(discord.FFmpegPCMAudio(returnRandomSound()))
+        await ctx.send(random.randint(1, 100))
+    except discord.ext.commands.CommandInvokeError:
+        await ctx.send(random.randint(1, 100))
+
 
 
 @R5.command('join')
@@ -17,13 +38,19 @@ async def join(ctx):
     vc = ctx.message.author.voice.channel
     try:
         await vc.connect()
+        voice = discord.utils.get(R5.voice_clients, guild=ctx.guild)
+        voice.play(discord.FFmpegPCMAudio(returnChannelSound()))
     except discord.ClientException:
         await ctx.send("BEEEEP!")
+        voice.play(discord.FFmpegPCMAudio(RES_DIR + err))
 
 
 @R5.command('leave')
 async def leave(ctx):
+    voice = discord.utils.get(R5.voice_clients, guild=ctx.guild)
+    voice.stop()
     try:
+        voice.play(discord.FFmpegPCMAudio(returnChannelSound()))
         await R5.voice_clients[0].disconnect()
     except discord.ClientException:
         await ctx.send("BEEEEP!")
