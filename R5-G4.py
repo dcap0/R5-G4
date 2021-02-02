9fd758e89a84a18614c5a477986917740a9fe357
Dennis <capone@alamon.com>
2021-02-02 10:47:08 -0700

    Added sounds to the query check. Added atmos command. Added sound checks to query and obligation

--- R5-G4.py
+++ R5-G4.py
@@ -7,7 +7,6 @@ from discord.ext import commands
 
 RES_DIR = os.path.dirname(os.path.abspath(__file__)) + "/res/"
 
-
 print("Vwooooo!")
 
 R5 = commands.Bot(command_prefix='R5:')
@@ -18,32 +17,37 @@ err = 'err.mp3'
 
 
 def returnChannelSound():
-    return RES_DIR + channelSounds[random.randint(0, 2)]
+    return RES_DIR + 'vcSounds/' + channelSounds[random.randint(0, 2)]
 
 
 def returnRandomSound():
-    return RES_DIR + randomSounds[random.randint(0, 7)]
+    return RES_DIR + 'rSounds/' + randomSounds[random.randint(0, 7)]
 
 
-@R5.command('speak', brief="R5 plays a random sound")
+def returnAtmosphericSound(arg: str):
+    return RES_DIR + 'atmosSounds/' + arg + '.mp3'
+
+
+@R5.command('speak', brief="*R5 plays a random sound")
 async def speak(ctx):
     R5.voice_clients[0].play(discord.FFmpegPCMAudio(returnRandomSound()))
 
 
 @R5.command('obligation', brief="R5 rolls a d100 to determine the obligation.")
 async def obligation(ctx):
-    voice = discord.utils.get(R5.voice_clients, guild=ctx.guild)
     try:
-        voice.stop()
-        voice.play(discord.FFmpegPCMAudio(returnRandomSound()))
-        await ctx.send(random.randint(1, 100))
+        if not R5.voice_clients[0].is_playing():
+            R5.voice_clients[0].play(discord.FFmpegPCMAudio(returnRandomSound()))
+            await ctx.send(random.randint(1, 100))
+        else:
+            await ctx.send(random.randint(1, 100))
     except discord.ext.commands.CommandInvokeError:
         await ctx.send(random.randint(1, 100))
     except AttributeError:
         await ctx.send(random.randint(1, 100))
 
 
-@R5.command('join', brief="Joins R5 to the voice channel.")
+@R5.command('join', brief="Joins R5 to the voice channel. * means voice only!")
 async def join(ctx):
     vc = ctx.message.author.voice.channel
     try:
@@ -56,7 +60,7 @@ async def join(ctx):
         voice.play(discord.FFmpegPCMAudio(RES_DIR + err))
 
 
-@R5.command('leave', brief="Kicks R5 from the voice channel.")
+@R5.command('leave', brief="*Kicks R5 from the voice channel.")
 async def leave(ctx):
     try:
         R5.voice_clients[0].stop()
@@ -72,13 +76,13 @@ async def leave(ctx):
         print("Sound played")
 
 
-@R5.command('play', brief="Plays a song that R5 has in his memory banks")
+@R5.command('play', brief="*Plays a song that R5 has in his memory banks")
 async def play(ctx, song: str):
     voice = discord.utils.get(R5.voice_clients, guild=ctx.guild)
     voice.play(discord.FFmpegPCMAudio(song))
 
 
-@R5.command('stop', brief="Stops R5 from playing any music.")
+@R5.command('stop', brief="*Stops R5 from playing any music/sounds.")
 async def stop(ctx):
     voice = discord.utils.get(R5.voice_clients, guild=ctx.guild)
     voice.stop()
@@ -87,7 +91,22 @@ async def stop(ctx):
 @R5.command('query', brief='Search databank for information about a subject. Encase in ""')
 async def query(ctx, arg: str):
     userquery = arg.replace('"', '').replace(" ", "_")
-    await ctx.message.author.send("https://starwars.fandom.com/wiki/" + userquery)
+    try:
+        if not R5.voice_clients[0].is_playing:
+            R5.voice_clients[0].play(discord.FFmpegPCMAudio(returnRandomSound()))
+            await ctx.message.author.send("https://starwars.fandom.com/wiki/" + userquery)
+        else:
+            await ctx.message.author.send("https://starwars.fandom.com/wiki/" + userquery)
+    except discord.ext.commands.CommandInvokeError:
+        await ctx.message.author.send("https://starwars.fandom.com/wiki/" + userquery)
+    except AttributeError:
+        await ctx.message.author.send("https://starwars.fandom.com/wiki/" + userquery)
+
+
+@R5.command('atmos', brief='*Plays atmospheric sounds. R5:help atmos to get list of sounds',
+            description="List of sound options will be here")
+async def query(ctx, arg: str):
+    R5.voice_clients[0].play(discord.FFmpegPCMAudio(returnAtmosphericSound(arg)))
 
 
 R5.run("")
