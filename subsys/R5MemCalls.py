39034a4c314835c19c7d222bb170e9565c0cb7c2
Dennis <capone@alamon.com>
2021-02-04 16:16:49 -0700

    Updated to get access keys from the config file

--- subsys/R5MemCalls.py
+++ subsys/R5MemCalls.py
@@ -1,13 +1,20 @@
 import pymongo
 from pymongo import MongoClient 
+from utility.R5config import DB_KEY
 
-R5MemInit = MongoClient("")
+R5MemInit = MongoClient(DB_KEY)
 db = R5MemInit["R5-test"]
 collection = db["test"]
 
-
 def dbtest():
     queryResult = collection.find({})
+    results = []
+    for q in queryResult:
+        results.append(str(q))
+    return results
+
+def dbtest2():
+    queryResult = collection.find({}).sort({"_id":-1}).limit(1)
     results = []
     for q in queryResult:
         results.append(str(q))
